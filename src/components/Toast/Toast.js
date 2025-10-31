39b26c1473e59fbba049f8d0e763ab972ba38bdc
web.jw <webjw.contact@gmail.com>
2025-10-31 11:27:06 +0100

    fix

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AlertOctagon, AlertTriangle, CheckCircle, Info, X } from 'react-feather';
 
 import VisuallyHidden from '../VisuallyHidden';
+import { ToastContext } from '../ToastProvider/ToastProvider';
 
 import styles from './Toast.module.css';
 
@@ -12,11 +13,13 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({ children, variant, hideToast }) {
+function Toast({ children, variant, id }) {
   if (!ICONS_BY_VARIANT[variant]) {
     const options = Object.keys(ICONS_BY_VARIANT).join(', ');
     throw Error(`The variant should be one of the following strings: ${options}`);
   }
+
+  const { removeToast } = React.useContext(ToastContext);
 
   const Icon = ICONS_BY_VARIANT[variant];
 
@@ -31,7 +34,12 @@ function Toast({ children, variant, hideToast }) {
         {children}
       </p>
 
-      <button aria-label='Dismiss message' aria-live='off' className={styles.closeButton} onClick={hideToast}>
+      <button
+        aria-label='Dismiss message'
+        aria-live='off'
+        className={styles.closeButton}
+        onClick={() => removeToast(id)}
+      >
         <X size={24} />
       </button>
     </div>
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -6,13 +6,13 @@ import styles from './ToastShelf.module.css';
 import { ToastContext } from '../ToastProvider/ToastProvider';
 
 function ToastShelf() {
-  const { toasts, removeToast } = React.useContext(ToastContext);
+  const { toasts } = React.useContext(ToastContext);
 
   return (
     <ol role='region' aria-live='polite' aria-label='Notification' className={styles.wrapper}>
       {toasts?.map((toast, index) => (
         <li className={styles.toastWrapper} key={toast.id}>
-          <Toast variant={toast.variant} hideToast={() => removeToast(toast.id)}>
+          <Toast variant={toast.variant} id={toast.id}>
             {toast.message}
           </Toast>
         </li>
